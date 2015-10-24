844a8e3e66b19e21754f3200b260bb44cc5b03b5
Abdulsattar Mohammed <asattar.md@gmail.com>
2015-10-24 18:52:41 +0530

    change styling

--- client/components/thumbnail.js
+++ client/components/thumbnail.js
@@ -3,9 +3,8 @@ import React from 'react';
 class Thumbnail extends React.Component {
     render() {
         return (<div className="thumbnail" onClick={this.props.onClick}>
-            <div className="image" style={{backgroundImage: `url('${this.props.image}')`}} title={this.props.title}></div>
-            <br/>
-            <span className="title">{this.props.title}</span>
+            <p className="image" style={{backgroundImage: `url('${this.props.image}')`}} title={this.props.title}></p>
+            <p className="title">{this.props.title}</p>
         </div>);
     }
 }
