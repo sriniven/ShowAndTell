4c59b3ad5f9f972aaf332d54a89edf336a008bbc
Abdulsattar Mohammed <asattar.md@gmail.com>
2015-11-02 17:50:59 +0530

    Fix click on iOS 8

--- client/components/thumbnail.js
+++ client/components/thumbnail.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 class Thumbnail extends React.Component {
     render() {
-        return (<div className="thumbnail" onClick={this.props.onClick}>
+        return (<a href="#" className="thumbnail" onClick={this.props.onClick}>
             <p className="image" style={{backgroundImage: `url('${this.props.image}')`}} title={this.props.title}></p>
             <p className="title">{this.props.title}</p>
-        </div>);
+        </a>);
     }
 }
 export default Thumbnail;
